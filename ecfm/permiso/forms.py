ba7938d1a09970820a6703f96360ac64ea210767
Hugo Garcia <hugoallangm@gmail.com>
2017-03-29 07:53:23 -0600

    Formulario casi listo

--- ecfm/permiso/forms.py
+++ ecfm/permiso/forms.py
@@ -1,10 +1,15 @@
+#-*-encoding:utf8-*-
 from django import forms
 import datetime
-
+MY_CHOICES = (
+    ('1', 'Docencia'),
+    ('2', 'Investigación'),
+    ('3', 'Extensión'),
+)
 class Permiso(forms.Form):
     registro_personal = forms.IntegerField()
     nombre_profesional = forms.CharField()
     nombre_evento = forms.CharField()
+    lugar_evento = forms.CharField()
     fecha_evento = forms.DateField(initial=datetime.date.today)
-
-    
+    Tipo_de_evento = forms.ChoiceField(choices=MY_CHOICES)
--- ecfm/permiso/views.py
+++ ecfm/permiso/views.py
@@ -1,17 +1,30 @@
+#-*-encoding:utf8-*-
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.template import loader
 from django.http import HttpResponse
 from .forms import Permiso
+import datetime
+import locale
 # Create your views here.
+formato_local = "%x "
+locale.setlocale(locale.LC_ALL, "es_GT.utf8")
 
 def procesar_permiso(request):
     if request.method == 'POST':
         form = Permiso(request.POST)
         if form.is_valid():
-            fichero = open('machote.tex','w')
+            fichero = open('permiso/machote/datos.tex','w')
             registro = form.cleaned_data['registro_personal']
-            fichero.write('Su men: ' + str(registro) )
+            fichero.write('\\def \\registro : { ' + str(registro) + ' }\n' )
+            nombre = form.cleaned_data['nombre_profesional']
+            fichero.write('\\def \\nombre : { ' + str(nombre) + ' }\n' )
+            nombre_evento = form.cleaned_data['nombre_evento']
+            fichero.write('\\def \\evento : { ' + str(nombre_evento) + ' }\n' )
+            fecha = form.cleaned_data['fecha_evento']
+            fichero.write('\\def \\dias : { ' + datetime.datetime.strftime(fecha,"%d %B de %Y") + ' }\n' )
+            lugar = form.cleaned_data['lugar_evento']
+            fichero.write('\\def \\lugar : { ' + str(lugar) + ' }\n' )
             template = loader.get_template('base.html')
             context ={}
             return HttpResponse(template.render(request))
